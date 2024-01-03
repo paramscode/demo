953a5e4ade6824e84416d61e0d32962d963adda4
Param Sandhu <sandhu.param@gmail.com>
2024-01-02 20:37:02 -0600

    new endpoints

--- src/main/java/com/paramsandhu/demo/controller/EmployeeController.java
+++ src/main/java/com/paramsandhu/demo/controller/EmployeeController.java
@@ -4,6 +4,8 @@ import com.paramsandhu.demo.dto.EmployeeRequest;
 import com.paramsandhu.demo.entity.Employee;
 import com.paramsandhu.demo.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,5 +20,26 @@ public class EmployeeController {
     public ResponseEntity<String> createEmployee(@RequestBody EmployeeRequest employeeRequest) {
         employeeRepository.save(employeeRequest.getEmployee());
         return ResponseEntity.ok("Employee saved successfully");
+    }
+
+    @GetMapping
+    public ResponseEntity<Iterable<Employee>> getAllEmployees() {
+        return ResponseEntity.ok(employeeRepository.findAll());
+    }
+
+    @GetMapping("/page/{pageNo}/{pageSize}")
+    public ResponseEntity<Iterable<Employee>> getAllEmployeesWithPagination(
+            @PathVariable("pageNo") Integer pageNo,
+            @PathVariable("pageSize") Integer pageSize) {
+
+        PageRequest pageable = PageRequest.of(pageNo, pageSize);
+        Page<Employee> page = employeeRepository.findAll(pageable);
+        return ResponseEntity.ok(page);
+        //return ResponseEntity.ok(employeeRepository.findAllWithPagination(pageNo, pageSize));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(employeeRepository.findById(id).get());
     }
 }
--- src/main/java/com/paramsandhu/demo/repository/EmployeeRepository.java
+++ src/main/java/com/paramsandhu/demo/repository/EmployeeRepository.java
@@ -1,9 +1,12 @@
 package com.paramsandhu.demo.repository;
 
 import com.paramsandhu.demo.entity.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     // Custom methods if required
+    Page<Employee> findAll(Pageable pageable);
 }
 
